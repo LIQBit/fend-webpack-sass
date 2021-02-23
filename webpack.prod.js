b4621514736f827c624ec2fe944db8591ee34ad0
Leigh Bitossi <leeroy77@gmailcom>
2021-02-23 20:42:38 +0900

    Trying to fix build-prod

--- webpack.prod.js
+++ webpack.prod.js
@@ -17,6 +17,10 @@ module.exports = {
                 use: [ 'style-loader', 'css-loader', 'sass-loader' ],
                 exclude: /node_modules/,
                 loader: "babel-loader"
+            },
+            {
+                test: /\.scss$/,
+                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
             }
         ]
     },
