cf3d387da15001a9403ef7b2e8956d4a4e17f8ac
Leigh Bitossi <leeroy77@gmailcom>
2021-02-28 18:55:53 +0900

    add terser, optimize css plugins

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,8 +1,14 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
+    optimization: {
+        minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
+    },
     entry: './src/client/index.js',
     mode: 'production',
     output: {
@@ -18,7 +24,7 @@ module.exports = {
             },
             {
                 test: /\.scss$/,
-                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+                use: [MiniCssExtractPlugin.loader,'style-loader', 'css-loader', 'sass-loader' ]
             }
         ]
     },
@@ -26,6 +32,8 @@ module.exports = {
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
-        })
+        }),
+        new MiniCssExtractPlugin({ filename: "[name].css"})
+
     ]
 }
